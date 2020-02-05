3286e58123765193eb7b05e01c08af9feb0ce02f
“Cory <corytyme@yahoo.com>
2020-02-05 07:15:28 -0500

    Created Avatar and AuthorRow components

--- App.js
+++ App.js
@@ -1,19 +1,27 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import Constants from 'expo-constants'
+
+import AuthorRow from './components/AuthorRow'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <AuthorRow
+        fullname={'First Last'}
+        linkText={'Comments'}
+        onPressLinkText={() => {
+            console.log('pressed')
+        }}
+    />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: Constants.statusBarHeight,
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
